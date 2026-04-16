a401c79b02a43740366d701aa2f991036dd48e15
lilianlacerda <lilianraiany@gmail.com>
2026-04-16 16:59:40 -0300

    inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// Nossa parte de lógica 
+		System.out.println("Hello World"); // Mesma coisa do escreva do portugol
+		
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,23 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		//Variaveis 
+		int idade = 25;
+		String nome = "Lilian";
+		float graus = 20.0f; 	//float => ponto flutuante
+		boolean resposta; 		// boolean => logico true/false
+		
+		//constantes 
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(nome);
+		System.out.println(QUANTIDADE);
+		
+
+	}
+
+}
